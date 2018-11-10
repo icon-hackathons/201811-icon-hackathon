a6638cb52911414b39ddb41d45575181d3043b75
Bongan Ha <unamed122@gmail.com>
2018-11-10 10:19:16 +0900

    generate contents

--- dpes/src/router.js
+++ dpes/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+
 
 Vue.use(Router);
 
@@ -8,16 +8,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'index',
+      component: () => import('./views/Index.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/organization',
+      name: 'organization',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Organization.vue'),
     },
   ],
 });
---
+++ dpes/src/views/Index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    <h1>Decentralized Performance Evaluation System</h1>
+  </div>
+</template>
